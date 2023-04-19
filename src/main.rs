136c5cc3d04179c6b65f4761e9dfa561b2e5b926
Shrecknt <58538423+Shrecknt@users.noreply.github.com>
2023-04-18 18:33:04 -0700

    Implement RconClient Use rust naming conventions

--- src/main.rs
+++ src/main.rs
@@ -4,18 +4,29 @@ use tokio::io::{AsyncWriteExt, AsyncReadExt};
 use tokio::net::TcpStream;
 
 mod rcon;
-use rcon::RCONPacket;
+use rcon::{RconPacket, RconClient};
 
 #[tokio::main]
 pub async fn main() -> Result<(), Box<dyn Error>> {
+
+    let mut connection = RconClient::connect("157.211.143.221:25575", "minecraft").await?;
+    let player_list_message = connection.command("list").await?;
+    println!("Got player list:\n{}", player_list_message);
+
     let stdin = std::io::stdin();
     let mut iterator = stdin.lock().lines();
 
-    let mut stream = TcpStream::connect("157.211.143.221:25575").await?;
+    println!("Enter the RCON server's ip:");
+    let input_ip = iterator.next().unwrap().unwrap();
+    let mut ip = "157.211.143.221:25575";
+    if !input_ip.eq("") {
+        ip = input_ip.as_str();
+    }
+    let mut stream = TcpStream::connect(ip).await?;
 
     let mut request_id: i32 = 0;
 
-    let mut login_packet = RCONPacket::new();
+    let mut login_packet = RconPacket::new();
     login_packet.request_id = request_id;
     login_packet.request_type = 3;
     println!("RCON client connected! Enter password:");
@@ -25,7 +36,7 @@ pub async fn main() -> Result<(), Box<dyn Error>> {
     let mut buf: Vec<u8> = vec![];
     stream.readable().await?;
     let _size = stream.try_read_buf(&mut buf).unwrap();
-    let mut return_packet = RCONPacket::new();
+    let mut return_packet = RconPacket::new();
     return_packet.parse(&buf).await?;
     return_packet.print();
     if return_packet.request_id == -1 {
@@ -37,7 +48,7 @@ pub async fn main() -> Result<(), Box<dyn Error>> {
     for line in iterator {
         let command: Vec<u8> = line.unwrap().into();
 
-        let mut send_packet = RCONPacket::new();
+        let mut send_packet = RconPacket::new();
         request_id += 1;
         send_packet.request_id = request_id;
         send_packet.request_type = 2;
@@ -48,7 +59,7 @@ pub async fn main() -> Result<(), Box<dyn Error>> {
         let mut res_buf: Vec<u8> = vec![];
         stream.readable().await?;
         let _size = stream.read_buf(&mut res_buf).await?;
-        let mut res_return_packet = RCONPacket::new();
+        let mut res_return_packet = RconPacket::new();
         res_return_packet.parse(&res_buf).await?;
         res_return_packet.print();
     }
--- src/rcon.rs
+++ src/rcon.rs
@@ -1,15 +1,89 @@
 use std::error::Error;
-use tokio::io::AsyncWriteExt;
+use tokio::{io::{AsyncWriteExt, AsyncReadExt}, net::TcpStream};
 
-pub struct RCONPacket {
+pub struct RconClient {
+    request_id: i32,
+    connected: bool,
+    stream: Option<TcpStream>
+}
+
+impl RconClient {
+    pub async fn connect(addr: &str, password: &str) -> Result<Self, Box<dyn Error>> {
+        let mut client = RconClient {
+            request_id: 0,
+            connected: false,
+            stream: None
+        };
+
+        let connection = TcpStream::connect(addr).await?;
+
+        client.stream = Some(connection);
+        client.connected = true;
+
+        let mut login_packet = RconPacket::new();
+        login_packet.request_id = client.request_id;
+        client.request_id += 1;
+        login_packet.request_type = 3;
+        login_packet.payload = password.into();
+
+        let login_res = client.send_packet(login_packet).await?;
+
+        if login_res.request_id == -1 {
+            client.connected = false;
+            client.stream = None;
+            panic!("Incorrect password");
+        } else if login_res.request_id != 0 {
+            panic!("are you sure you connected to an rcon server?");
+        }
+
+        Ok(client)
+    }
+
+    async fn send_packet(&mut self, packet: RconPacket) -> Result<RconPacket, Box<dyn Error>> {
+        if !&self.connected {
+            panic!("some error message");
+        }
+        match &mut self.stream {
+            Some(stream) => {
+                let built_packet = packet.build().await?;
+                stream.write_all(&built_packet).await?;
+
+                let mut res_buf: Vec<u8> = vec![];
+                stream.readable().await?;
+                let _size = stream.read_buf(&mut res_buf).await?;
+                let mut res_return_packet = RconPacket::new();
+                res_return_packet.parse(&res_buf).await?;
+
+                return Ok(res_return_packet);
+            }
+            None => {
+                panic!("How did you get here??");
+            }
+        }
+    }
+
+    pub async fn command(&mut self, command: &str) -> Result<String, Box<dyn Error>>{
+        let mut packet = RconPacket::new();
+        packet.request_id = self.request_id;
+        self.request_id += 1;
+        packet.request_type = 2;
+        packet.payload = command.into();
+
+        let res = self.send_packet(packet).await?;
+
+        Ok(res.payload_to_string())
+    }
+}
+
+pub struct RconPacket {
     pub request_id: i32,
     pub request_type: i32,
     pub payload: Vec<u8>
 }
 
-impl RCONPacket {
+impl RconPacket {
     pub fn new() -> Self {
-        RCONPacket {
+        RconPacket {
             request_id: 0,
             request_type: 0,
             payload: vec![]
@@ -30,8 +104,8 @@ impl RCONPacket {
     }
 
     pub async fn parse(&mut self, raw: &Vec<u8>) -> Result<(), Box<dyn Error>> {
-        let length = as_i32_le(&raw[0..4].try_into().unwrap());
-        println!("Packet Length: {}", length);
+        let _length = as_i32_le(&raw[0..4].try_into().unwrap());
+        //println!("Packet Length: {}", _length);
         let request_id = as_i32_le(&raw[4..8].try_into().unwrap());
         let request_type = as_i32_le(&raw[8..12].try_into().unwrap());
         let mut payload = (&raw[13..]).to_vec();
