7d774edded66f0622afcf4b3d2729c4ce5d293e0
Shrecknt <58538423+Shrecknt@users.noreply.github.com>
2023-04-18 20:34:57 -0700

    Use RconClient

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,11 @@
 use std::error::Error;
 use std::io::BufRead;
-use tokio::io::{AsyncWriteExt, AsyncReadExt};
-use tokio::net::TcpStream;
 
 mod rcon;
-use rcon::{RconPacket, RconClient};
+use rcon::RconClient;
 
 #[tokio::main]
 pub async fn main() -> Result<(), Box<dyn Error>> {
-    let mut connection = RconClient::connect("157.211.143.221:25575", None).await?;
-    connection.login("minecraft").await?;
-    let player_list_message = connection.command("list").await?;
-    println!("Got player list:\n{}", player_list_message);
-
     let stdin = std::io::stdin();
     let mut iterator = stdin.lock().lines();
 
@@ -24,53 +17,32 @@ pub async fn main() -> Result<(), Box<dyn Error>> {
         ip = input_ip.as_str();
     }
 
-    let mut stream = TcpStream::connect(ip).await?;
-
-    let mut request_id: i32 = 0;
-
-    let mut login_packet = RconPacket::new();
-    login_packet.request_id = request_id;
-    login_packet.request_type = 3;
+    let mut connection = RconClient::connect(ip, None).await?;
 
     let default_password = "minecraft";
     println!("RCON client connected! Enter password (default '{}'):", default_password);
-    login_packet.payload = default_password.into();
     let input_password = iterator.next().unwrap().unwrap();
+    let mut password = default_password;
     if !input_password.eq("") {
-        login_packet.payload = input_password.into();
+        password = input_password.as_str();
     }
 
-    stream.write_all(&login_packet.build().await?).await?;
+    let return_packet = connection.login(password).await;
 
-    let mut buf: Vec<u8> = vec![];
-    stream.readable().await?;
-    let _size = stream.try_read_buf(&mut buf).unwrap();
-    let mut return_packet = RconPacket::new();
-    return_packet.parse(&buf).await?;
-    return_packet.print();
-    if return_packet.request_id == -1 {
-        println!("Connection failed :(");
-    } else {
-        println!("Connected :)");
-    }
+    match return_packet {
+        Ok(_return_packet) => {
+            println!("Connected :)");
+            for line in iterator {
+                let command = line.unwrap();
 
-    for line in iterator {
-        let command: Vec<u8> = line.unwrap().into();
+                let result = connection.command(command.as_str()).await?;
 
-        let mut send_packet = RconPacket::new();
-        request_id += 1;
-        send_packet.request_id = request_id;
-        send_packet.request_type = 2;
-        send_packet.payload = command;
-
-        stream.write_all(&send_packet.build().await?).await?;
-
-        let mut res_buf: Vec<u8> = vec![];
-        stream.readable().await?;
-        let _size = stream.read_buf(&mut res_buf).await?;
-        let mut res_return_packet = RconPacket::new();
-        res_return_packet.parse(&res_buf).await?;
-        res_return_packet.print();
+                println!("{}", result);
+            }
+        }
+        Err(err) => {
+            panic!("Connection failed: {}", err);
+        }
     }
 
     Ok(())
