853c10df9e6ecb76853a309658662de686a8bbe0
Shrecknt <58538423+Shrecknt@users.noreply.github.com>
2023-04-26 11:03:15 -0700

    Client join function

--- src/client.rs
+++ src/client.rs
@@ -1,6 +1,7 @@
 use std::error::Error;
+use tokio::{io::AsyncWriteExt, net::TcpStream};
 
-use tokio::net::TcpStream;
+use crate::packetutil::{get_packet, send_prefixed_packet};
 
 pub struct Client {
     host: String,
@@ -8,20 +9,55 @@ pub struct Client {
     connection: Option<TcpStream>,
 }
 
+pub struct ClientJoinData {
+    pub response_id: i32,
+    pub buffer: Vec<u8>,
+}
+
 impl Client {
-    pub fn new(host: String, port: Option<u16>) -> Self {
-        Client {
+    pub async fn connect(host: String, port: Option<u16>) -> Result<Self, Box<dyn Error>> {
+        let mut res = Client {
             host,
             port: port.unwrap_or(25565),
             connection: None,
-        }
+        };
+
+        res.connection = Some(TcpStream::connect(format!("{}:{}", res.host, res.port)).await?);
+
+        Ok(res)
     }
 
-    pub async fn connect(&mut self) -> Result<(), Box<dyn Error>> {
-        if self.connection.is_some() {
-            return Err("Already connected".into());
+    pub async fn join(&mut self) -> Result<ClientJoinData, Box<dyn Error>> {
+        match &mut self.connection {
+            Some(stream) => {
+                let protocol_version: usize = 0xf805;
+                let hostname = "shrecked.dev";
+                let port = 25565;
+
+                let mut connect_packet: Vec<u8> = vec![];
+                connect_packet.write_u8(0x00).await?;
+                varint_rs::VarintWriter::write_usize_varint(&mut connect_packet, protocol_version)?; // protocol version - 760 (1.19.2)
+                varint_rs::VarintWriter::write_usize_varint(&mut connect_packet, hostname.len())?; // host length - 12
+                connect_packet.write(hostname.as_bytes()).await?; // host name - shrecked.dev
+                connect_packet.write_u16(port).await?; // port number - 42069
+                connect_packet.write_u8(0x02).await?; // next state - 1 (login)
+
+                send_prefixed_packet(stream, &connect_packet).await?;
+
+                let mut login_start_packet: Vec<u8> = vec![];
+                login_start_packet.write_u8(0x00).await?;
+
+                send_prefixed_packet(stream, &login_start_packet).await?;
+
+                let result = get_packet(stream).await?;
+                return Ok(ClientJoinData {
+                    response_id: result.packet_id,
+                    buffer: result.buffer,
+                });
+            }
+            None => {
+                return Err("No connection, cannot join".into());
+            }
         }
-        self.connection = Some(TcpStream::connect(format!("{}:{}", self.host, self.port)).await?);
-        Ok(())
     }
 }
