d99bfb87fae98ecceed66f61f9e933fb02e6ffcd
Shrecknt <58538423+Shrecknt@users.noreply.github.com>
2023-04-28 20:27:16 -0700

    Connect to server?

--- src/client.rs
+++ src/client.rs
@@ -30,24 +30,36 @@ impl Client {
     pub async fn join(&mut self) -> Result<ClientJoinData, Box<dyn Error>> {
         match &mut self.connection {
             Some(stream) => {
-                let protocol_version: usize = 0xf805;
+                let protocol_version: usize = 760; // 0xf805;
                 let hostname = "shrecked.dev";
                 let port = 25565;
+                let playername = "Shrecknt";
 
                 let mut connect_packet: Vec<u8> = vec![];
                 connect_packet.write_u8(0x00).await?;
                 varint_rs::VarintWriter::write_usize_varint(&mut connect_packet, protocol_version)?; // protocol version - 760 (1.19.2)
-                varint_rs::VarintWriter::write_usize_varint(&mut connect_packet, hostname.len())?; // host length - 12
+                varint_rs::VarintWriter::write_usize_varint(
+                    &mut connect_packet,
+                    hostname.as_bytes().len(),
+                )?; // host length - 12
                 connect_packet.write(hostname.as_bytes()).await?; // host name - shrecked.dev
                 connect_packet.write_u16(port).await?; // port number - 42069
-                connect_packet.write_u8(0x02).await?; // next state - 1 (login)
+                connect_packet.write_u8(0x02).await?; // next state - 2 (login)
 
                 send_prefixed_packet(stream, &connect_packet).await?;
 
                 let mut login_start_packet: Vec<u8> = vec![];
                 login_start_packet.write_u8(0x00).await?;
+                varint_rs::VarintWriter::write_usize_varint(
+                    &mut login_start_packet,
+                    playername.as_bytes().len(),
+                )?;
+                login_start_packet.write(playername.as_bytes()).await?;
+                login_start_packet.write_u8(0x01).await?;
 
                 send_prefixed_packet(stream, &login_start_packet).await?;
+
+                println!("{:?}", stream);
 
                 let result = get_packet(stream).await?;
                 return Ok(ClientJoinData {
